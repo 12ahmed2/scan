5270deae461269f7c96b00b041542af9d585a6d9
Ahmed Mohamed <69980108+12ahmed2@users.noreply.github.com>
2025-08-25 17:35:54 +0300

    Update scanner.js

--- scanner.js
+++ scanner.js
@@ -68,19 +68,19 @@ function scanFrame() {
   animationFrameId = requestAnimationFrame(scanFrame);
 }
 
-function getUserIdFromUrl() {
-  const params = new URLSearchParams(window.location.search);
-  return params.get("uid");
-}
+// function getUserIdFromUrl() {
+//   const params = new URLSearchParams(window.location.search);
+//   return params.get("uid");
+// }
 
-const uid = getUserIdFromUrl();
-if (uid) {
-  alert("UID from QR link: " + uid);
-  // fetch(`/api/user/${uid}`) ...
-}
+// const uid = getUserIdFromUrl();
+// if (uid) {
+//   alert("UID from URL: " + uid);
+// }
 
 // Attach button listeners
 startButton.addEventListener('click', startScanning);
 stopButton.addEventListener('click', stopScanning);
+
 
 
